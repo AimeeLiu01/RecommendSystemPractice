fa754d8f43844e8b12007f55737f197cfeb3c477
AimeeLiu01 <978611061@qq.com>
2020-02-09 19:57:31 +0800

    Initial commit

---
+++ Chapter2/UserCF.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: liuchang
+@software: PyCharm
+@file: UserCF.py
+@time: 2020-02-09 19:55
+@注释：基于用户的协同过滤算法
+'''
+
---
+++ Chapter2/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: liuchang
+@software: PyCharm
+@file: __init__.py.py
+@time: 2020-02-09 19:53
+'''
